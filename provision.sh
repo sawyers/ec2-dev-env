6147da65086c491a8b296b8c46e7d3231404256b
Michael <5084015+sawyers@users.noreply.github.com>
2021-05-20 14:54:10 -0700

    update version of git

--- provision.sh
+++ provision.sh
@@ -1,14 +1,14 @@
 #! /bin/bash -xe
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-  yum install -y git libselinux-python3.x86_64 yum-utils
+  yum install -y libselinux-python3.x86_64 yum-utils
   yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
   yum install -y containerd.io docker-ce docker-ce-cli
   usermod -aG docker centos
   systemctl enable --now docker
   chmod 666 /var/run/docker.sock
   # installing pyenv
-  yum -y install epel-release
-  yum -y install gcc zlib-devel bzip2-devel readline-devel sqlite-devel openssl-devel
+  yum -y install epel-release https://repo.ius.io/ius-release-el7.rpm 
+  yum -y install git224 gcc zlib-devel bzip2-devel readline-devel sqlite-devel openssl-devel
   runuser -l centos -c "curl https://pyenv.run | bash"
   echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~centos/.bashrc
   echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~centos/.bashrc
